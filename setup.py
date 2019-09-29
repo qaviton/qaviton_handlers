3aa881630e3fa254bd72942126b19ebead708078
yehonadav <yonadav.barilan@gmail.com>
2019-09-29 09:41:04 +0300

    build candidate 2019.9.29.6.41.4.335461

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with open("README.md", encoding="utf8") as f: long_description = f.read()
     setup(
         name=package_name,
-        version="2019.9.29.6.22.10.839064",
+        version="2019.9.29.6.41.4.335461",
         author="yehonadav",
         author_email="yonadav.barilan@gmail.com",
         description="qaviton handlers",
